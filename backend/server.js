7e63c95cbee23f9e01199b4c01f663fe9f08c955
ZahrahNaveed <zahrahnaveed.1911@gmail.com>
2025-07-15 03:22:37 +0500

    Tested redirect from short code

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,30 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const Url = require("./models/url.model");
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+
+// Redirect short code
+app.get("/:shortCode", async (req, res) => {
+  const { shortCode } = req.params;
+  try {
+    const found = await Url.findOne({ shortCode });
+    if (!found) return res.status(404).send("URL not found");
+
+    found.accessCount += 1;
+    await found.save();
+
+    res.redirect(found.url);
+  } catch (err) {
+    res.status(500).send("Server Error");
+  }
+});
 
 // Routes
 const urlRoutes = require("./routes/url.routes");
